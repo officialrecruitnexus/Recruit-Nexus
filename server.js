cf2a50e87e08c3181de00350ce38a40e1ec0313d
officialrecruitnexus <official.recruitnexus@gmail.com>
2026-06-05 22:09:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/jobseeker-dashboard.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'jobseeker-dashboard.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
